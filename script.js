4a8e96fd72e2fd6c7a9b7592e0795b3ef7094ba9
Mario Lisbona <mario.lisbona@gmail.com>
2022-06-30 19:17:51 +1000

    logic for dynamic tooltip pos

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const shareButton = document.querySelector('.social-share-container');
-const tooltip = document.querySelector('.content');
+const content = document.querySelector('.tooltip-content');
 const arrow = document.querySelector('.arrow');
+const tooltip = document.querySelector('.tooltip');
+
+window.addEventListener('DOMContentLoaded', contenPosition);
+window.addEventListener('resize', contenPosition);
 
 shareButton.addEventListener('mouseover', () => {
-    tooltip.classList.add('active');
+    content.classList.add('active');
     arrow.classList.add('active');
 });
 
 window.addEventListener('click', () => {
-    tooltip.classList.remove('active');
+    content.classList.remove('active');
     arrow.classList.remove('active');
 });
+
+function contenPosition () {
+    const shareButton = tooltip.querySelector('.social-share-container')
+    const ttContent = tooltip.querySelector('.tooltip-content')
+    const arrow = tooltip.querySelector('.arrow');
+
+    // ttContent.style.left = shareButton.offsetLeft + -80 + 'px';
+    ttContent.style.left = shareButton.offsetLeft - ttContent.offsetWidth / 2 + 'px';
+    ttContent.style.top = shareButton.offsetTop + -80 + 'px';
+    arrow.style.left = shareButton.offsetLeft - ttContent.offsetLeft + shareButton.offsetWidth/2 + 'px';
+};
