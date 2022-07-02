a64e61d4bc77d8d2c4c7de67fa93bdb50d6d57c3
Mario Lisbona <mario.lisbona@gmail.com>
2022-07-02 14:11:14 +1000

    add comments to script

--- script.js
+++ script.js
@@ -3,25 +3,30 @@ const content = document.querySelector('.tooltip-content');
 const arrow = document.querySelector('.arrow');
 const tooltip = document.querySelector('.tooltip');
 
+//window eventlisters to call the contentPosition function after all teh DOM elements have been loaded
+//and call it every time the window is resized
 window.addEventListener('DOMContentLoaded', contenPosition);
 window.addEventListener('resize', contenPosition);
 
+
+//eventlistener on share button to add the active class to display the tooltip content and arrow
 shareButton.addEventListener('mouseover', () => {
     content.classList.add('active');
     arrow.classList.add('active');
 });
 
+//eventlistener that removes the active class from the tooltip content and arrow on any click in the window
 window.addEventListener('click', () => {
     content.classList.remove('active');
     arrow.classList.remove('active');
 });
 
+//function to set the position of the tooltip and arrow elements
 function contenPosition () {
     const shareButton = tooltip.querySelector('.social-share-container')
     const ttContent = tooltip.querySelector('.tooltip-content')
     const arrow = tooltip.querySelector('.arrow');
 
-    // ttContent.style.left = shareButton.offsetLeft + -80 + 'px';
     ttContent.style.left = shareButton.offsetLeft - ttContent.offsetWidth / 2 + 'px';
     ttContent.style.top = shareButton.offsetTop + -80 + 'px';
     arrow.style.left = shareButton.offsetLeft - ttContent.offsetLeft + shareButton.offsetWidth/2 + 'px';
